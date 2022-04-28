387e9adc87636b1ec9050821db57ccfb06fa36ed
Ignacio Espinosa <emprendiendoem@gmail.com>
2022-04-28 12:44:32 -0400

    Revisión final

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'projects/dashboard'
   get 'projects/form'
+  get 'projects/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
